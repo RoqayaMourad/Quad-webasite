d4791feb8762b1799f5831463ccf6d3830caa131
roqaya299 <elephantsong29@gmail.com>
2021-06-08 04:00:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//     $("#teamimg").hover(
+//       function () {
+//         $(this).addClass(" animate__animated animate__wobble ");
+//       },
+//       function () {
+//         $(this).removeClass(" animate__animated animate__wobble");
+//       }
+//     );
+//   });

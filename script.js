286e40c5e026744ca076fa0906ee21b8781f1105
roqaya299 <elephantsong29@gmail.com>
2021-06-09 06:56:30 -0700

    breathing animation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // $(document).ready(function () {
-//     $("#teamimg").hover(
+//     $(".full-img").hover(
 //       function () {
-//         $(this).addClass(" animate__animated animate__wobble ");
+//         $(".img11").addClass(" animate__animated animate__bounce animate__slow");
+//         $(".img12").addClass(" animate__animated animate__bounce animate__slow");
 //       },
 //       function () {
-//         $(this).removeClass(" animate__animated animate__wobble");
+//         $(".img11").removeClass(" animate__animated animate__bounce ");
+//         $(".img12").removeClass(" animate__animated animate__bounce ");
 //       }
 //     );
 //   });
